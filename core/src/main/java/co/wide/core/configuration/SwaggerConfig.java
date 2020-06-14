8624195e2aadcc38561b8c72cf24291e90accdc9
artemsukhov <gow55511@yandex.ru>
2020-06-14 18:01:58 +0700

    swagger

--- core/src/main/java/co/wide/core/configuration/SwaggerConfig.java
+++ core/src/main/java/co/wide/core/configuration/SwaggerConfig.java
@@ -3,16 +3,16 @@ package co.wide.core.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-
-import java.util.Collections;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
@@ -26,14 +26,10 @@ public class SwaggerConfig {
     }
 
     private ApiInfo getApiInfo() {
-        return new ApiInfo(
-                "Wide API",
-                "Simple API",
-                "1.0",
-                "For free use",
-                new Contact("wide team", "wide-team.ru", "wide-team@gmail.ru"),
-                "license",
-                "wide-team.ru",
-                Collections.emptyList());
+        return new ApiInfoBuilder()
+                .title("Wide card API")
+                .description("Simple API")
+                .build();
     }
+
 }
