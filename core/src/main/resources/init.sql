ef81e081aadb4be378eaef0caf17904ec70e84c6
Aldar Shoyd <aldartron@gmail.com>
2020-07-19 00:49:33 +0700

    пофиксил скрипт

--- core/src/main/resources/init.sql
+++ core/src/main/resources/init.sql
@@ -1,4 +1,4 @@
 -- TODO: Перенести в механизм миграции, когда появится (flyway/liquibase)
 create database wide;
-create person widecore with encrypted password 'widecore';
+create user widecore with encrypted password 'widecore';
 grant all privileges on database wide to widecore;
